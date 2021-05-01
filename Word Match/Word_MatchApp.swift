b690936fdbf6d83b025f6cba6e99c2d4d081464d
Evgeniy Ivon <ivoneug@gmail.com>
2021-05-01 11:52:32 +0300

    Initial Commit

---
+++ Word Match/Word_MatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  Word_MatchApp.swift
+//  Word Match
+//
+//  Created by Evgeniy on 01.05.2021.
+//
+
+import SwiftUI
+
+@main
+struct Word_MatchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
